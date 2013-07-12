e6aebebbc6601dde69fd557b8853dc6ab335cbca
Ubuntu <ubuntu@ip-172-31-17-7.us-west-2.compute.internal>
2013-07-12 21:07:52 +0000

    Read Test

--- web.js
+++ web.js
@@ -6,6 +6,12 @@ app.get('/', function(request, response) {
   response.send('Hello World 2!');
 });
 
+var fs = require('fs');
+fs.readFile('index.html', function(err, data) {
+	if (err) throw err;
+	console.log(data);
+}
+
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
