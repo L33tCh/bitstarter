a0f5200cfdc3d9084bcd9442c7dc77ada33a8ab1
Ubuntu <ubuntu@ip-172-31-17-7.us-west-2.compute.internal>
2013-07-13 12:54:07 +0000

    Read Test

--- web.js
+++ web.js
@@ -5,26 +5,15 @@ var app = express.createServer(express.logger());
 var fs = require('fs');
 
 var buf = new Buffer(fs.readFileSync('index.html'));
-//buf.write("Hello", "utf-8");
-//buf.write(fs.readFileSync('/index.html')
+
 var t = buf.toString("utf-8");
 
 app.get('/', function(request, response) {
   response.send(t);//'Hello World 2!');
 });
 
-
-
-//console.log(buf.length);
-
-//var fs = require('fs');
-//fs.readFileSync('/index.html', function(err, data) {
-//	if (err) throw err;
-//	buf = new Buffer(data);
-//	buffer.write();
-//});
-
 var port = process.env.PORT || 5000;
+
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
